76564b546ed98fef770c25f188ac6a1550ab7ef8
mustang_ex <wojtek@wojciechxtx.com>
2024-06-10 10:48:17 +0000

    update repo url

--- app/Domain/Github/GithubStars.php
+++ app/Domain/Github/GithubStars.php
@@ -6,7 +6,7 @@ class GithubStars
     public function fetchStars(): ?int
     {
         $result = @\file_get_contents(
-            'https://api.github.com/repos/pradoslaw/coyote',
+            'https://api.github.com/repos/MrSuddenJoy/4programmers.net',
             false,
             \stream_context_create([
                 'http' => ['header' => 'User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'],
